af40519ea7e62542ccbee43133aa4283d0a06c54
Sebastian Christ <rudolfo.christ@gmail.com>
2014-11-09 15:35:41 +0100

    Fetch and write movie data

--- data-fetcher.lisp
+++ data-fetcher.lisp
@@ -1,7 +1,10 @@
 
 (in-package :cl-user)
 (defpackage :data-fetcher
-  (:use :cl :drakma :yason))
+  (:nicknames :df)
+  (:use :cl :drakma)
+  (:export :fetch-data
+           :write-data))
 
 (in-package :data-fetcher)
 
@@ -10,3 +13,18 @@
 (defvar *movie-ids-path* (merge-pathnames *default-pathname-defaults* "movie-ids.txt"))
 
 (defvar *omdb-url* "http://www.imdbapi.com/?" "The Open Movie Database API URL")
+
+(defun fetch-data ()
+  (let ((data nil))
+    (with-open-file (file *movie-ids-path*)
+      (loop for line = (read-line file nil 'eof)
+            until (eq line 'eof)
+            do (multiple-value-bind (json)
+                   (http-request (format nil "~Ai=~A&r=json" *omdb-url* line))
+                 (push json data))))
+    data))
+
+(defun write-data ()
+  (let ((data (fetch-data)))
+    (with-open-file (file *output-path* :direction :output :if-does-not-exist :create)
+      (format file "[~{~a~^,~}]" data))))
