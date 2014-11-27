f5ca4a157a5842aff67cf220159da9a1865501b4
Sebastian Christ <rudolfo.christ@gmail.com>
2014-11-27 17:23:32 +0100

    Add more lucene deps

---
+++ java/index-backend/src/main/java/de/hsmannheim/iws2014/indexing/Searcher.java
@@ -0,0 +1,11 @@
+package de.hsmannheim.iws2014.indexing;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: fyi
+ * Date: 11/27/14
+ * Time: 5:00 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Searcher {
+}
