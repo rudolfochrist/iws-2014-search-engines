e2100594371b9cc62afdefeefdf8a367e1e4d6b1
Sebastian Christ <rudolfo.christ@gmail.com>
2014-11-27 17:23:47 +0100

    Add rudimentary searcher

--- java/index-backend/src/main/java/de/hsmannheim/iws2014/indexing/Searcher.java
+++ java/index-backend/src/main/java/de/hsmannheim/iws2014/indexing/Searcher.java
@@ -1,11 +1,19 @@
 package de.hsmannheim.iws2014.indexing;
 
+import org.apache.lucene.search.IndexSearcher;
+
 /**
- * Created with IntelliJ IDEA.
- * User: fyi
- * Date: 11/27/14
- * Time: 5:00 PM
- * To change this template use File | Settings | File Templates.
+ * Search for indexed Developers.
  */
 public class Searcher {
+
+    private IndexSearcher index;
+
+    public Searcher(IndexSearcher index) {
+        this.index = index;
+    }
+
+    public void search(String queryString) {
+
+    }
 }
