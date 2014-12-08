b22958efb53d300e1f2746a0183b109b472b6a03
Sebastian Christ <rudolfo.christ@gmail.com>
2014-12-08 21:09:01 +0100

    Implement pig latin conversion

--- java/index-backend/src/main/java/de/hsmannheim/iws2014/analyzers/PigLatinFilter.java
+++ java/index-backend/src/main/java/de/hsmannheim/iws2014/analyzers/PigLatinFilter.java
@@ -2,8 +2,11 @@ package de.hsmannheim.iws2014.analyzers;
 
 import org.apache.lucene.analysis.TokenFilter;
 import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Actually transforms each token.
@@ -12,12 +15,29 @@ public final class PigLatinFilter extends TokenFilter {
 
     public static final String VOCALS = "aeio";
 
+    private final CharTermAttribute term;
+
     public PigLatinFilter(TokenStream stream) {
         super(stream);
+        this.term = addAttribute(CharTermAttribute.class);
     }
 
     @Override
     public boolean incrementToken() throws IOException {
+        if (!input.incrementToken()) return false;
+
+        StringBuilder sTerm = new StringBuilder(term);
+        char firstChar = sTerm.charAt(0);
+
+        if(!VOCALS.contains(""+firstChar)) {
+            sTerm.deleteCharAt(0);
+            sTerm.append(firstChar);
+        }
+
+        sTerm.append("ay");
+        term.setEmpty();
+        term.append(sTerm);
+
         return true;
     }
 }
